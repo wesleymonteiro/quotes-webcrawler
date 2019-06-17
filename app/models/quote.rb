e096794bd2d1fddf205529572257aaf2f7d787ec
Unknown <weslito96@gmail.com>
2019-06-17 19:39:49 -0300

    create new project

---
+++ app/models/quote.rb
@@ -0,0 +1,7 @@
+class Quote
+  include Mongoid::Document
+  field :quote, type: String
+  field :author, type: String
+  field :author_about, type: String
+  field :tags, type: Array, default: []
+end
